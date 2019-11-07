9a1fc05cb0fcfe358e4d83e7284a689bfd50c604
Guillaume Jouvencel <guillaume.jouvencel@econocom.com>
2019-11-07 14:42:51 +0100

    Test firebase

--- app/src/main/java/com/example/zerodechet/MainActivity.kt
+++ app/src/main/java/com/example/zerodechet/MainActivity.kt
@@ -1,30 +1,30 @@
 package com.example.zerodechet
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.Button
 import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
 
 class MainActivity : AppCompatActivity(), TaskRowListener {
-    override fun onTaskChange(objectId: String, isDone: Boolean) {
-            val task = _db.child(Statics.FIREBASE_TASK).child(objectId).child("done")
-            task.setValue(isDone)
-    }
-    override fun onTaskDelete(objectId: String) {
-        val task = _db.child(Statics.FIREBASE_TASK).child(objectId)
-        task.removeValue()
-    }
+
     lateinit var _db: DatabaseReference
     lateinit var _adapter: TaskAdapter
     var _taskList: MutableList<Task>? = null
+    var _fbAuth = FirebaseAuth.getInstance()
     var _taskListener: ValueEventListener = object : ValueEventListener {
         override fun onDataChange(dataSnapshot: DataSnapshot) {
             loadTaskList(dataSnapshot)
@@ -35,10 +35,22 @@ class MainActivity : AppCompatActivity(), TaskRowListener {
         }
     }
 
+    override fun onTaskChange(objectId: String, isDone: Boolean) {
+        val task = _db.child(Statics.FIREBASE_TASK).child(objectId).child("done")
+        task.setValue(isDone)
+    }
+    override fun onTaskDelete(objectId: String) {
+        val task = _db.child(Statics.FIREBASE_TASK).child(objectId)
+        task.removeValue()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        var btnLogin = findViewById<Button>(R.id.btnLogin)
+        btnLogin.setOnClickListener {view ->
+            signIn(view,"alex@ynov.com", "mdpmdp")
+        }
         _db = FirebaseDatabase.getInstance().reference
         _taskList = mutableListOf()
         _adapter = TaskAdapter(this, _taskList!!)
@@ -135,5 +147,23 @@ class MainActivity : AppCompatActivity(), TaskRowListener {
         _adapter.notifyDataSetChanged()
 
     }
+    fun signIn(view: View,email: String, password: String){
+        showMessage(view,"Authenticating...")
 
+        _fbAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, OnCompleteListener<AuthResult> { task ->
+            if(task.isSuccessful){
+                var intent = Intent(this, LoggedInActivity::class.java)
+                intent.putExtra("id", _fbAuth.currentUser?.email)
+                startActivity(intent)
+
+            }else{
+                showMessage(view,"Error: ${task.exception?.message}")
+            }
+        })
+
+    }
+
+    fun showMessage(view:View, message: String){
+        Snackbar.make(view, message, Snackbar.LENGTH_INDEFINITE).setAction("Action", null).show()
+    }
 }
